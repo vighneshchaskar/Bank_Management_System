3acd3cf12e4b6d3300a92c16391d437a14a37fda
Vighnesh Chaskar <154252547+vighneshchaskar@users.noreply.github.com>
2024-08-23 20:25:47 +0530

    Add files via upload

---
+++ src/BankingSystem/User.java
@@ -0,0 +1,104 @@
+package BankingSystem;
+
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class User {
+    private Connection connection;
+    private Scanner scanner;
+
+    public User(Connection connection, Scanner scanner){
+        this.connection = connection;
+        this.scanner = scanner;
+    }
+
+    public void register(){
+
+        //register the user
+        scanner.nextLine();
+        System.out.print("Full Name: ");
+        String full_name = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        //check if user is existed or not
+        if(user_exist(email)) {
+            System.out.println("User Already Exists for this Email Address!!");
+            return;
+        }
+
+        //inserting  data into database
+        String register_query = "INSERT INTO User(full_name, email, password) VALUES(?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(register_query);
+            preparedStatement.setString(1, full_name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("Registration Successfull!");
+            } else {
+                System.out.println("Registration Failed!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //CHECKING IS USER EXIT OR NOT
+    public boolean user_exist(String email){
+        String query = "SELECT * FROM user WHERE email = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+
+
+    //LOGIN
+    public String login(){ //string because we want to share email throught the function.
+        scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        String login_query = "SELECT * FROM User WHERE email = ? AND password = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(login_query);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return email;          //user exist so we share email
+            }else{
+                return null;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
